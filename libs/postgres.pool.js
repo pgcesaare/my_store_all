b463e995c4fb1e920bba049f973a2d30fd5b9ffe
pgcesaare <cesarelias.ponceg@gmail.com>
2024-09-26 14:17:27 -0700

    Cambios Realizados

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -2,12 +2,22 @@ const { Pool } = require('pg');
 
 const { config } = require('./../config/config')
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
+let URI = '';
 
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+if (config.isProd) {
+  URI = config.dbUrl;
+}
+else {
+    const USER = encodeURIComponent(config.dbUser);
+    const PASSWORD = encodeURIComponent(config.dbPassword);
+
+    const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+}
 
 const pool = new Pool({ connectionString: URI });
+    
+
 
 module.exports = pool;
 
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,14 +2,28 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config')
 const  setupModels  = require('./../db/models')
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+let URI = '';
 
-const sequelize = new Sequelize(URI,{
+if (config.isProd) {
+  URI = config.dbUrlProd;
+}
+else {
+    URI=config.dbUrlDev;
+}
+
+const options = {
     dialect: 'postgres',
-    logging: true,
-});
+    logging: config.isProd ? false : true
+}
+
+if (config.isProd) {
+
+    options.ssl = {
+        rejectUnauthorized: false
+    }
+}
+
+const sequelize = new Sequelize(URI,options);
 
 setupModels(sequelize);
 
