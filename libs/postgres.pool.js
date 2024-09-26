6b6f13d3eae9b90355ea971592a71ffff7055cb2
pgcesaare <cesarelias.ponceg@gmail.com>
2024-09-26 14:57:21 -0700

    Fix error in connection

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -3,19 +3,26 @@ const { Pool } = require('pg');
 const { config } = require('./../config/config')
 
 let URI = '';
+const options = {
+    connectionString: URI,
+}
 
 if (config.isProd) {
   URI = config.dbUrl;
+  options.connectionString = config.Url
+  options.ssl = {
+    rejectUnauthorized: false
+  }
 }
 else {
     const USER = encodeURIComponent(config.dbUser);
     const PASSWORD = encodeURIComponent(config.dbPassword);
-
     const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+    options.connectionString = URI;
 
 }
 
-const pool = new Pool({ connectionString: URI });
+const pool = new Pool(options);
     
 
 
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -17,9 +17,10 @@ const options = {
 }
 
 if (config.isProd) {
-
-    options.ssl = {
-        rejectUnauthorized: false
+    options.dialectOptions = {
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 }
 
